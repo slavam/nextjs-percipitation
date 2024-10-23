4ede6bbfa846b2b4064ee98e5c35950c8a72941a
slavam <mwm1955@gmail.com>
2024-10-23 13:17:39 +0300

    Daily temperature for Amvrosievka fixed

--- app/dashboard/temperature/page.js
+++ app/dashboard/temperature/page.js
@@ -1,4 +1,5 @@
-import Search from '@/app/ui/search'   
+import Search from '@/app/ui/search'
+import { lusitana } from '@/app/ui/fonts'
 
 const absoluteZero = 273.15
 export const metadata = {
@@ -8,8 +9,13 @@ export default async function Page({searchParams}) {
   const reportDate = searchParams?.reportDate || new Date().toISOString().slice(0,10)
   let reportDateSec = Math.round(new Date(reportDate).getTime()/1000)
   const points='0,10800,21600,32400,43200,54000,64800,75600'
-  let data = await fetch(`http://10.54.1.30:8640/get?quality=1&sources=100,10202&hashes=795976906,1451382247&points=${points}&stations=34519,34524,34622,34721,34615,34712&notbefore=${reportDateSec}&notafter=${reportDateSec+22*60*60}`)
-  let observations = await data.json()
+  let data = await fetch(`http://10.54.1.30:8640/get?quality=1&sources=100,10202&hashes=795976906,1451382247&point=${points}&stations=34519,34524,34622,34721,34615,34712&notbefore=${reportDateSec}&notafter=${reportDateSec+22*60*60}`)
+  let observations = []
+  try {
+    observations = await data.json()
+  }catch (error) {
+    console.error('Database Error:', error);
+  }
   const codeStations = [null,34519,34524,34622,34721,34615,34712]
   const starts = [0,3,6,9,12,15,18,21]
   const startTerms = [
@@ -23,7 +29,8 @@ export default async function Page({searchParams}) {
         {label:'21', value:7},
       ]
   let shiftTerms = starts //starts.slice(startTerm.value).concat(starts.slice(0,startTerm.value))
-    let row0 = ['Срок'].concat(shiftTerms).concat(['Средняя','N'])
+    // let row0 = [''].concat(shiftTerms).concat(['Средняя','N'])
+    let row0 = shiftTerms //.concat(['','',''])
     let i,j
     let temps = [row0,
                  ['Донецк',null,null,null,null,null,null,null,null,0,0,0],
@@ -41,10 +48,24 @@ export default async function Page({searchParams}) {
           temps[i][j] = +tempC
           temps[i][9]+= +tempC
           temps[i][10]+=1
+          temps[i][11] = +o.id
         }else if(+o.station===34622){
           temps[i][j] = +tempC
           temps[i][9]+= +tempC
           temps[i][10]+=1
+          temps[i][11] = +o.id
+        }
+      }else if(+o.meas_hash===795976906){
+        if(((+o.value-absoluteZero)!== +temps[i][j])&&(+o.id>temps[i][11])){
+          temps[i][11] = +o.id
+          temps[i][9] = +temps[i][9]-(+temps[i][j])+(+tempC)
+          temps[i][j] = +tempC
+        }
+      }else if(+o.station===34622){
+        if(((+o.value-absoluteZero)!== +temps[i][j])&&(+o.id>temps[i][11])){
+          temps[i][11] = +o.id
+          temps[i][9] = +temps[i][9]-(+temps[i][j])+(+tempC)
+          temps[i][j] = +tempC
         }
       }
     })
@@ -70,6 +91,7 @@ export default async function Page({searchParams}) {
     let content = <div >
       <table className="hidden min-w-full text-gray-900 md:table">
         <thead className="rounded-md bg-gray-400 text-left text-sm font-normal">
+          <tr><th rowSpan='2'>Метеостанции</th><th colSpan={8} align='center'>Сроки</th><th rowSpan='2'>Средняя</th><th rowSpan='2'>N</th><th></th><th></th></tr>
           <tr>{liveHead}</tr>
         </thead>
         {myBody}
@@ -77,12 +99,16 @@ export default async function Page({searchParams}) {
     </div>
   return (
     <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Среднесуточная температура</h1>
+      </div>
+      {/* <h2>Отчетная дата {reportDateSec} {reportDate}</h2> */}
       <h2>Отчетная дата</h2>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search />
       </div>
       <div className="mt-6 flow-root">
-        <h1>Температура воздуха (°С) {reportDate} в сроки наблюдений, начиная со срока 00 по данным метеорологических станций</h1>
+        <h1>Температура воздуха (°С) {reportDate} в сроки наблюдений по данным метеорологических станций</h1>
         <div className="bg-gray-50 p-2 md:pt-0">
           {content}
         </div>
